de90fdbd845bd2312719401fd60d1b99c683f59b
Geobio Boo <geobio.boo@airbnb.com>
2013-12-16 18:09:12 -0800

    more crap

--- src/com/geobioboo/sensortag/TipOverIntentService.java
+++ src/com/geobioboo/sensortag/TipOverIntentService.java
@@ -1,11 +1,16 @@
 package com.geobioboo.sensortag;
 
+import java.util.UUID;
+
 import ti.android.ble.common.BluetoothLeService;
 import ti.android.ble.sensortag.R;
+import ti.android.ble.sensortag.Sensor;
 import android.app.IntentService;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -15,6 +20,7 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 public class TipOverIntentService extends IntentService {
@@ -24,10 +30,14 @@ public class TipOverIntentService extends IntentService {
     private BluetoothDevice mBluetoothDevice = null;
     private BluetoothLeService mBluetoothLeService = null;
 
+    private BluetoothLeService mBtLeService = null;
+    private BluetoothGatt mBtGatt = null;
+    private static final int GATT_TIMEOUT = 100; // milliseconds
+
     private static boolean mIsRunning;
 
-    public TipOverIntentService(String name) {
-        super(name);
+    public TipOverIntentService() {
+        super("bleh");
     }
 
     @Override
@@ -37,10 +47,19 @@ public class TipOverIntentService extends IntentService {
             registerReceiver(mBluetoothStateChangeReceiver, getBluetoothIntentFilter());
 
             startBluetoothLeService();
+
+            mBtLeService = BluetoothLeService.getInstance(); // TODO maybe need a callback to init these!
+
+            mBtGatt = BluetoothLeService.getBtGatt();
+
+            registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+
+            mIsRunning = true;
         }
     }
 
     private boolean checkForBluetoothLe() {
+        Log.d("geobio", "tip checkForBluetoothLe");
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_LONG).show();
             return false;
@@ -67,6 +86,7 @@ public class TipOverIntentService extends IntentService {
     }
 
     private void startBluetoothLeService() {
+        Log.d("geobio", "tip startBluetoothLeService");
 
         Intent bindIntent = new Intent(this, BluetoothLeService.class);
         startService(bindIntent);
@@ -75,6 +95,7 @@ public class TipOverIntentService extends IntentService {
     }
 
     private void scanLeDevice(boolean enable) {
+        Log.d("geobio", "tip scanLeDevice " + enable);
         if (enable) {
             mBtAdapter.startLeScan(mLeScanCallback);
         } else {
@@ -82,7 +103,7 @@ public class TipOverIntentService extends IntentService {
         }
     }
 
-    private IntentFilter getBluetoothIntentFilter() {
+    private static IntentFilter getBluetoothIntentFilter() {
 
         IntentFilter mFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         mFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
@@ -94,7 +115,7 @@ public class TipOverIntentService extends IntentService {
 
         @Override
         public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
-
+            Log.d("geobio", "tip onLeScan");
             if (isSensorTag(device)) {
                 mBluetoothDevice = device;
                 connect();
@@ -104,6 +125,7 @@ public class TipOverIntentService extends IntentService {
 
     private void connect() {
 
+        Log.d("geobio", "tip connect");
         int connState = mBluetoothManager.getConnectionState(mBluetoothDevice, BluetoothGatt.GATT);
 
         switch (connState) {
@@ -133,6 +155,7 @@ public class TipOverIntentService extends IntentService {
             final String action = intent.getAction();
 
             if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                Log.d("geobio", "tip state change onReceive state changed");
                 // Bluetooth adapter state change
                 switch (mBtAdapter.getState()) {
                     case BluetoothAdapter.STATE_ON:
@@ -145,12 +168,14 @@ public class TipOverIntentService extends IntentService {
                 }
 
             } else if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                Log.d("geobio", "tip state change onReceive gatt connected");
                 // GATT connect
                 int status = intent.getIntExtra(BluetoothLeService.EXTRA_STATUS, BluetoothGatt.GATT_FAILURE);
                 if (status == BluetoothGatt.GATT_SUCCESS) {
                     // TODO then do stuff in DeviceActivity
                 }
             } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                Log.d("geobio", "tip state change onReceive gatt disconnected");
                 // GATT disconnect
                 int status = intent.getIntExtra(BluetoothLeService.EXTRA_STATUS, BluetoothGatt.GATT_FAILURE);
                 //                stopDeviceActivity();
@@ -170,6 +195,7 @@ public class TipOverIntentService extends IntentService {
 
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder service) {
+            Log.d("geobio", "tip service connected connected");
             mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
             mBluetoothLeService.initialize();
             scanLeDevice(true);
@@ -178,7 +204,73 @@ public class TipOverIntentService extends IntentService {
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
+            Log.d("geobio", "tip service connection disconnected");
             mBluetoothLeService = null;
+        }
+    };
+
+    /* **************************************************************
+    ************************************************************ */
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter fi = new IntentFilter();
+        fi.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        fi.addAction(BluetoothLeService.ACTION_DATA_NOTIFY);
+        fi.addAction(BluetoothLeService.ACTION_DATA_WRITE);
+        fi.addAction(BluetoothLeService.ACTION_DATA_READ);
+        return fi;
+    }
+
+    private void enableSensors(boolean enable) {
+        Sensor sensor = Sensor.ACCELEROMETER; // the only sensor I want
+
+        UUID servUuid = sensor.getService();
+        UUID confUuid = sensor.getConfig();
+
+        BluetoothGattService serv = mBtGatt.getService(servUuid);
+        BluetoothGattCharacteristic charac = serv.getCharacteristic(confUuid);
+        byte value = enable ? sensor.getEnableSensorCode() : Sensor.DISABLE_SENSOR_CODE;
+        mBtLeService.writeCharacteristic(charac, value);
+        mBtLeService.waitIdle(GATT_TIMEOUT);
+
+    }
+
+    private void enableNotifications(boolean enable) {
+        Sensor sensor = Sensor.ACCELEROMETER;
+        UUID servUuid = sensor.getService();
+        UUID dataUuid = sensor.getData();
+        BluetoothGattService serv = mBtGatt.getService(servUuid);
+        BluetoothGattCharacteristic charac = serv.getCharacteristic(dataUuid);
+
+        mBtLeService.setCharacteristicNotification(charac, enable);
+        mBtLeService.waitIdle(GATT_TIMEOUT);
+
+    }
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("geobio", "tip ** broadcasts receiver received!");
+            final String action = intent.getAction();
+            int status = intent.getIntExtra(BluetoothLeService.EXTRA_STATUS, BluetoothGatt.GATT_SUCCESS);
+
+            if (BluetoothLeService.ACTION_DATA_NOTIFY.equals(action)) {
+                // Notification
+                byte[] value = intent.getByteArrayExtra(BluetoothLeService.EXTRA_DATA);
+                String uuidStr = intent.getStringExtra(BluetoothLeService.EXTRA_UUID);
+
+                if (uuidStr.substring(4, 7).toLowerCase().equals("aa4")) {
+                    int baroValue = (int) (Sensor.BAROMETER.convert(value).x / 100); // approx 1000
+                    //                    if (supposedToBeEnabledSensors.contains("aa4")) {
+                    //                        // then need to remove
+                    //                        if (baroValue > 100) {
+                    //                            supposedToBeEnabledSensors.remove(uuidStr.substring(4, 7));
+                    //                        }
+                    //                    }
+
+                }
+            }
+
         }
     };
 
---
+++ src/ti/android/ble/sensortag/MainActivity.java
@@ -0,0 +1,37 @@
+package ti.android.ble.sensortag;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.geobioboo.sensortag.TipOverIntentService;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.service).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("geobio", "main act start service");
+                startService(new Intent(MainActivity.this, TipOverIntentService.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
